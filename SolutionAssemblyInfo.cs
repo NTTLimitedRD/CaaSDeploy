53419afd5aaa899b63428d1863c204fafe17a979
Pascal Enz <pascal.enz@itaas.dimensiondata.com>
2015-10-27 09:53:59 +1100

    Added versioning to solution.

---
+++ SolutionAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Dimension Data")]
+[assembly: AssemblyProduct("Compute as a Service (CaaS) Template Deploy.")]
+[assembly: AssemblyCopyright("Copyright © Dimension Data 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0-develop")]
