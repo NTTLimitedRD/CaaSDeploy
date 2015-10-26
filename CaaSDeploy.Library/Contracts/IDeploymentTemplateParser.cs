36ba627120d1b3b2be697d22ced19fa116682f19
Pascal Enz <pascal.enz@itaas.dimensiondata.com>
2015-10-26 15:08:11 +1100

    Replaced wrong interfaces.

---
+++ CaaSDeploy.Library/Contracts/IDeploymentTemplateParser.cs
@@ -0,0 +1,28 @@
+﻿using DD.CBU.CaasDeploy.Library.Models;
+using DD.CBU.CaasDeploy.Library.Tasks;
+
+namespace DD.CBU.CaasDeploy.Library.Contracts
+{
+    /// <summary>
+    /// Builds task lists and contexts from deployment template documents.
+    /// </summary>
+    public interface IDeploymentTemplateParser
+    {
+        /// <summary>
+        /// Gets the deployment tasks for the supplied deployment template.
+        /// </summary>
+        /// <param name="accountDetails">The CaaS account details.</param>
+        /// <param name="templateFilePath">The template file path.</param>
+        /// <param name="parametersFilePath">The parameters file path.</param>
+        /// <returns>Instance of <see cref="TaskExecutor"/> with tasks and task execution context.</returns>
+        TaskExecutor GetDeploymentTasks(CaasAccountDetails accountDetails, string templateFilePath, string parametersFilePath);
+
+        /// <summary>
+        /// Gets the deletion tasks for the supplied deployment log.
+        /// </summary>
+        /// <param name="accountDetails">The CaaS account details.</param>
+        /// <param name="deploymentLogFilePath">The deployment log file path.</param>
+        /// <returns>Instance of <see cref="TaskExecutor"/> with tasks and task execution context.</returns>
+        TaskExecutor GetDeletionTasks(CaasAccountDetails accountDetails, string deploymentLogFilePath);
+    }
+}
